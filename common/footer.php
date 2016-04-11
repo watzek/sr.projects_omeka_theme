05f024e20816bc3e07da573f7474667dd87830f0
EC2 Default User <ec2-user@ip-172-31-30-86.us-west-2.compute.internal>
2016-04-11 17:51:08 +0000

    updated footer. some changes in header

--- common/footer.php
+++ common/footer.php
@@ -1,34 +1,39 @@
 </div>
 </div>
 	<div id="footer">
-	
-		
-		    <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
+	    <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
         <p><?php echo $footerText; ?></p>
         <?php endif; ?>
-    <?php fire_plugin_hook('public_footer'); ?>
+    	<?php fire_plugin_hook('public_footer'); ?>
 		<ul class="pagination">
 	
 		</ul>
 		<div class="row">
+			<div class="footer-menu large-4 columns">			
+				<h4>Lewis & Clark College</h4>
+				<p>
+					0615 SW Palatine Hill Road <br/>
+					Portland, Oregon 97219 USA <br />
+				</p>
+			</div>
 			<div class="footer-menu large-4 columns">
-			
-			<h4>About</h4>
-		<ul class="link-list">
-			<li><a href="http://omeka.org" id="omeka-logo" name="omeka-logo">Powered by Omeka</a></li> <li> <a href="http://omeka.org/codex/">Documentation</a></li> <li> <a href="http://omeka.org/forums/">Support Forums</a></li>
-		</ul>
-		</div>
-		<div class="footer-menu large-4 columns">
-			<h4>Menu</h4>
-			<ul class="link-list"
-			<?php
-echo public_nav_main();
-?></ul>
+				<h4>Menu</h4>
+				<ul class="link-list no-bullet">
+					<li><a href="/items/browse">Browse Items</a></li>
+					 <li><a href="/collections/browse">Browse Collections</a></li>
+					<?php //echo public_nav_main(); ?>
+				</ul>
 			</div>
 			
 			<div class="footer-menu large-4 columns">
-		<h4>Credits </h4>
-				</div>
+				<h4>Credits </h4>
+				<p>
+					Designed By <br />
+					<a href="http://library.lclark.edu/">Watzek Library</a><br />
+					<a href="mailto:librarian@lclark.edu">librarian@lclark.edu</a><br />
+					506-768-7274<br/>
+				</p>
+			</div>
 	</div>
 </div>
 </div>
