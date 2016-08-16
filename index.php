6ff5a8fd6fd692a5d9bc9db4a0c482c093905cf9
Rishi Javia <rishi.javia.3895@gmail.com>
2016-08-16 10:46:53 -0700

    stupid quotes

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ endif;
     foreach ($collections as $col) {
       set_current_record('collection', $col);
       echo '<div class="featured-col">';
-        echo '<a href ="/items/browse?collection=".echo metadata('collection', 'id');.">';
+        echo '<a href ="/items/browse?collection="'.metadata('collection', 'id').'">';
         echo '<img src='.metadata('collection', array('Dublin Core', 'Source')).' />';
         echo '</a>';
         echo link_to_items_browse(__(metadata('collection', array('Dublin Core', 'Title'))), array('collection' => metadata('collection', 'id')));
