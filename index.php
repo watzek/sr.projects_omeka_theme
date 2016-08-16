baf56ba8a0602326cada1e5fb5ba2c1254630bb1
Rishi Javia <rishi.javia.3895@gmail.com>
2016-08-16 10:36:25 -0700

    same

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ endif;
     foreach ($collections as $col) {
       set_current_record('collection', $col);
       echo '<div class="featured-col">';
-        echo '<a href='.link_to_items_browse(array('collection' => metadata('collection', 'id'))).'><img src='.metadata('collection', array('Dublin Core', 'Source')).' /></a>';
+        echo '<a href='.link_to_items_browse(__(array('collection' => metadata('collection', 'id')))).'><img src='.metadata('collection', array('Dublin Core', 'Source')).' /></a>';
         echo link_to_items_browse(__(metadata('collection', array('Dublin Core', 'Title'))), array('collection' => metadata('collection', 'id')));
       echo '</div>';
     }
