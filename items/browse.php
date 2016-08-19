f4720084bd2ad48278adbccdfe7f750c7bae3510
budak <thatbudakguy@gmail.com>
2016-08-19 10:51:01 -0700

    user mysql 5.6 as workaround for mounting /var/lib/mysql issue

--- items/browse.php
+++ items/browse.php
@@ -22,7 +22,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 			<h5 class="text-center"><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h5>
 			<p class="text-center"><strong>Author: </strong><?php echo metadata('item', array('Dublin Core', 'Creator')); ?> </p>
 			<p class="text-center"><strong>Year Published: </strong><?php echo metadata('item', array('Dublin Core', 'Date')); ?></p>
-    		<p class="text-center"><strong>Department: </strong><?php 
+    		<p class="text-center"><strong>Department: </strong><?php
     		if(metadata('item','Collection Name')):
     			echo link_to_items_browse(metadata('item','Collection Name'), array('collection' => metadata('item', 'Collection ID')));
     		endif; ?></p>
@@ -44,7 +44,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
     			<h4><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h4>
     			<p><strong>Author: </strong><?php echo metadata('item', array('Dublin Core', 'Creator')); ?> </p>
     			<p><strong>Year Published: </strong><?php echo metadata('item', array('Dublin Core', 'Date')); ?></p>
-    			<p><strong>Department: </strong><?php 
+    			<p><strong>Department: </strong><?php
     				if(metadata('item','Collection Name')):
     					echo link_to_items_browse(metadata('item','Collection Name'), array('collection' => metadata('item', 'Collection ID')));
     				endif;
